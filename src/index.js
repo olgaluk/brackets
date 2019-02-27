3f2c981cfc742b1eee44402f2d3a3730d5bd1f33
Olga Luk <olga_ivanova_86@mail.ru>
2019-02-27 21:15:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairsBrackets = bracketsConfig;
+  const strParam = str;
+  let strArray = strParam.split('');
+
+  for (let i = 0; i < pairsBrackets.length; i++) {
+    for (let j = 0; j < strArray.length; j++) {
+     while ((strArray[j] === pairsBrackets[i][0]) &&  (strArray[j+1] === pairsBrackets[i][1])) {
+      strArray.splice(j, 2);
+      j = 0;
+      i = 0;
+     }
+    }
+  }
+
+  return !strArray.length;
 }
